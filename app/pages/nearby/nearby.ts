a018d3c7ba01b0e445fa3a13aa79ffda7a7345c7
Jeff Pelletier <jeff.pelletier@intridea.com>
2016-06-24 14:48:22 -0400

    Core pages

---
+++ app/pages/nearby/nearby.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the NearbyPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/nearby/nearby.html',
+})
+export class NearbyPage {
+  constructor(private nav: NavController) {}
+}
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,7 +1,9 @@
-import {Component} from '@angular/core'
+import {Component} from '@angular/core';
 import {HomePage} from '../home/home';
-import {AboutPage} from '../about/about';
-import {ContactPage} from '../contact/contact';
+import {InboxPage} from '../inbox/inbox';
+import {NearbyPage} from '../nearby/nearby';
+import {ProfilePage} from '../profile/profile';
+import {SearchPage} from '../search/search';
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -11,12 +13,16 @@ export class TabsPage {
   private tab1Root: any;
   private tab2Root: any;
   private tab3Root: any;
+  private tab4Root: any;
+  private tab5Root: any;
 
   constructor() {
     // this tells the tabs component which Pages
     // should be each tab's root Page
     this.tab1Root = HomePage;
-    this.tab2Root = AboutPage;
-    this.tab3Root = ContactPage;
+    this.tab2Root = NearbyPage;
+    this.tab3Root = SearchPage;
+    this.tab4Root = ProfilePage;
+    this.tab5Root = InboxPage;
   }
 }
