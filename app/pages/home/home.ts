bca32d37462c5e4fbb1e31914ba4b0a3efa1e65c
Jeff Pelletier <jeff.pelletier@intridea.com>
2016-06-27 16:55:30 -0400

    Use FriendStatus provider in home view

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,15 +1,22 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {FriendStatus} from '../../providers/friend-status/friend-status';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  providers: [FriendStatus]
 })
 
 export class HomePage {
 
   private homeSegments = 'friends';
+  private statii = [];
 
-  constructor(private navController: NavController) {
-
+  constructor(private navController: NavController, private friendStatusService: FriendStatus) {
+    this.friendStatusService.getFriendStatii().then((statii) => {
+      if(statii) {
+        this.statii = statii;
+      }
+    });
   }
 }
