60c75e464388b811b04f9cad41eec1081aae8a22
Jeff Pelletier <jeff.pelletier@intridea.com>
2016-06-29 08:59:46 -0400

    Initial markup and style for jumbotron cards

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ gulp.task('build', ['clean'], function(done){
 });
 
 gulp.task('images', function() {
-  gulp.src('app/img/**/*.png')
+  gulp.src('app/img/**/*.{png,jpg}')
   .pipe(gulp.dest('www/build/img/'));
 });
 
