1b4f9f37c43f8139890f7c2b1d52134da9e1d4d2
Jeff Pelletier <jeff.pelletier@intridea.com>
2016-06-29 10:01:34 -0400

    Nearby is functional using mock data

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,21 +1,29 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {FriendStatus} from '../../providers/friend-status/friend-status';
+import {NearbyStatus} from '../../providers/nearby-status/nearby-status';
 
 @Component({
   templateUrl: 'build/pages/home/home.html',
-  providers: [FriendStatus]
+  providers: [FriendStatus, NearbyStatus]
 })
 
 export class HomePage {
 
   private homeSegments = 'friends';
-  private statii = [];
+  private friendStatii = [];
+  private nearbyStatii = [];
 
-  constructor(private navController: NavController, private friendStatusService: FriendStatus) {
+  constructor(private navController: NavController, private friendStatusService: FriendStatus, private nearbyStatiiService: NearbyStatus) {
     this.friendStatusService.getFriendStatii().then((statii) => {
-      if(statii) {
-        this.statii = statii;
+      if (statii) {
+        this.friendStatii = statii;
+      }
+    });
+
+    this.nearbyStatiiService.getNearbyStatii().then((statii) => {
+      if (statii) {
+        this.nearbyStatii = statii;
       }
     });
   }
