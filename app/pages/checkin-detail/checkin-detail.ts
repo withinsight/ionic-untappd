7f4b48ef1710b13bdaf2a1524dbeb3049e83112f
Jeff Pelletier <jeff.pelletier@intridea.com>
2016-06-30 15:11:56 -0400

    Primary UI for checkin detail

--- app/pages/checkin-detail/checkin-detail.ts
+++ app/pages/checkin-detail/checkin-detail.ts
@@ -6,24 +6,32 @@ import { NavParams } from 'ionic-angular';
 })
 
 export class CheckinDetailPage {
+  private avatar;
   private name;
   private userName;
   private userLocation;
   private beerName;
   private breweryName;
+  private beerIcon;
   private beerStyle;
-  private checkinLocation;
+  private checkinDate;
+  private checkinLocationName;
+  private checkinLocationAddress;
   private checkinLocationCategory;
 
   constructor(private navParams: NavParams) {
 
+    this.avatar = this.navParams.get('checkin').avatar;
     this.name = this.navParams.get('checkin').name;
     this.userName = this.navParams.get('checkin').userName;
     this.userLocation = this.navParams.get('checkin').userLocation;
     this.beerName = this.navParams.get('checkin').beerName;
     this.breweryName = this.navParams.get('checkin').breweryName;
+    this.beerIcon = this.navParams.get('checkin').beerIcon;
     this.beerStyle = this.navParams.get('checkin').beerStyle;
-    this.checkinLocation = this.navParams.get('checkin').location;
+    this.checkinDate = this.navParams.get('checkin').date;
+    this.checkinLocationName = this.navParams.get('checkin').locationName;
+    this.checkinLocationAddress = this.navParams.get('checkin').locationAddress;
     this.checkinLocationCategory = this.navParams.get('checkin').locationCategory;
 
   }
